7351e1c5fab21c5121b644c2ea6792e6d26f7c0f
Ricardo <rickbrum123@gmail.com>
2022-07-16 23:57:01 -0300

    projeto criado

---
+++ backend/src/test/java/com/checkList/demo/CheckListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkList.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
